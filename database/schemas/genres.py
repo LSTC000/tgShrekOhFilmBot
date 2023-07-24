220b27e033665010565bdd8c7cce40e6f5a9ed96
Ivan <razgonyaev_74@mail.ru>
2023-07-24 15:48:17 +0700

    update genres.py

--- database/schemas/genres.py
+++ database/schemas/genres.py
@@ -7,7 +7,7 @@ class Genres(BaseModel):
     __tablename__ = 'genres'
 
     # Genre id.
-    genre_id = Column(BigInteger, primary_key=True, nullable=False)
+    genre_id = Column(Integer, primary_key=True, nullable=False)
     # Genre name.
     genre_name = Column(VARCHAR(32), nullable=True)
     # Created review date.
