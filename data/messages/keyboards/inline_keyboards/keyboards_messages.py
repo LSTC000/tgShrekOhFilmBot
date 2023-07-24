d423194e9044a95df8612ac2821626ec0cebaeff
Ivan <razgonyaev_74@mail.ru>
2023-07-24 16:31:11 +0700

    update keyboards_messages.py

--- data/messages/keyboards/inline_keyboards/keyboards_messages.py
+++ data/messages/keyboards/inline_keyboards/keyboards_messages.py
@@ -3,8 +3,10 @@ CANCEL_TO_MAIN_MENU_IKB_MESSAGE = '⬅️ Вернуться в главное �
 # USERS COMMANDS.
 START_IKB_MESSAGE = 'Давай начнём 🚀'
 # MAIN MENU.
+USER_REVIEW_IKB_MESSAGE = '✏ Оставить отзыв'
 USER_ALERT_ON_IKB_MESSAGE = '🔔 Включить уведомления'
 USER_ALERT_OFF_IKB_MESSAGE = '🔕 Отключить уведомления'
+SOCIAL_IKB_MESSAGE = '🚀 Ещё больше о нас'
 PAYMENT_IKB_MESSAGE = '💰 Поддержать проект'
 # ADMIN MENU.
 ALERT_FOR_USERS_IKB_MESSAGE = '🗞️ Отправить объявление'
@@ -15,6 +17,5 @@ YOOMONEY_PAYMENT_IKB_MESSAGE = 'YooMoney'
 YOOMONEY_PAYMENT_URL_IKB_MESSAGE = 'Оплатить'
 DONATION_ALERTS_PAYMENT_URL_IKB_MESSAGE = 'Donation Alerts'
 # SOCIALS.
-SOCIAL_IKB_MESSAGE = '🚀 Ещё больше о нас'
 SOCIAL_TIK_TOK_CHANNEL_URL_IKB_MESSAGE = '📱 Наш TikTok'
 SOCIAL_OFFICIAL_TG_GROUP_IKB_MESSAGE = '📨 Наша группа в telegram'
