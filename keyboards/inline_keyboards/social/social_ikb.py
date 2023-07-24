e148a0afff62ac39b3cdf28b7ab53c66bda8d648
Ivan <razgonyaev_74@mail.ru>
2023-07-24 22:33:17 +0700

    rename SOCIAL_TIK_TOK_CHANNEL_URL_IKB_MESSAGE -> SOCIAL_TIK_TOK_SHREK_OH_FILM_CHANNEL_URL_IKB_MESSAGE

--- keyboards/inline_keyboards/social/social_ikb.py
+++ keyboards/inline_keyboards/social/social_ikb.py
@@ -4,7 +4,7 @@ from data.callbacks import CANCEL_TO_MAIN_MENU_CALLBACK_DATA
 
 from data.messages import (
     CANCEL_TO_MAIN_MENU_IKB_MESSAGE,
-    SOCIAL_TIK_TOK_CHANNEL_URL_IKB_MESSAGE,
+    SOCIAL_TIK_TOK_SHREK_OH_FILM_CHANNEL_URL_IKB_MESSAGE,
     SOCIAL_OFFICIAL_TG_GROUP_IKB_MESSAGE
 )
 
@@ -20,7 +20,7 @@ def social_ikb() -> InlineKeyboardMarkup:
 
     ikb = InlineKeyboardMarkup(row_width=ROW_WIDTH)
 
-    ikb.row(InlineKeyboardButton(text=SOCIAL_TIK_TOK_CHANNEL_URL_IKB_MESSAGE, url=TIK_TOK_SHREK_OH_FILM_CHANNEL_URL))
+    ikb.row(InlineKeyboardButton(text=SOCIAL_TIK_TOK_SHREK_OH_FILM_CHANNEL_URL_IKB_MESSAGE, url=TIK_TOK_SHREK_OH_FILM_CHANNEL_URL))
     ikb.row(InlineKeyboardButton(text=SOCIAL_OFFICIAL_TG_GROUP_IKB_MESSAGE, url=OFFICIAL_GROUP_URL))
     ikb.row(InlineKeyboardButton(text=CANCEL_TO_MAIN_MENU_IKB_MESSAGE, callback_data=CANCEL_TO_MAIN_MENU_CALLBACK_DATA))
 
