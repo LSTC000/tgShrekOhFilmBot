9cd716cff21d879b1a6e4630f88a96fdcedc0278
Ivan <razgonyaev_74@mail.ru>
2023-07-24 16:17:34 +0700

    add add_user_review.py

---
+++ database/commands/add_user_review.py
@@ -0,0 +1,19 @@
+from loader import logger
+
+from asyncpg import UniqueViolationError
+
+from database import Reviews
+
+
+async def add_user_alert(user_id: int, review: str) -> None:
+    '''
+    :param user_id: Telegram user id.
+    :param review: User review.
+    :return: None.
+    '''
+
+    try:
+        review = Reviews(user_id=user_id, review=review)
+        await review.create()
+    except UniqueViolationError:
+        logger.info('Error to user review! User review already exists in the database.')
