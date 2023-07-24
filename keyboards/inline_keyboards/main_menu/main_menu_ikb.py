c96b68351c8ed1f32b7e351309fd96df5782c66f
Ivan <razgonyaev_74@mail.ru>
2023-07-24 16:38:36 +0700

    update main_menu_ikb.py

--- keyboards/inline_keyboards/main_menu/main_menu_ikb.py
+++ keyboards/inline_keyboards/main_menu/main_menu_ikb.py
@@ -3,13 +3,15 @@ from data.config import ROW_WIDTH
 from data.callbacks import (
     CHANGE_USER_ALERT_CALLBACK_DATA,
     PAYMENT_CALLBACK_DATA,
-    SOCIAL_CALLBACK_DATA
+    SOCIAL_CALLBACK_DATA,
+    USER_REVIEW_CALLBACK_DATA
 )
 
 from data.messages import (
     USER_ALERT_ON_IKB_MESSAGE,
     USER_ALERT_OFF_IKB_MESSAGE,
     SOCIAL_IKB_MESSAGE,
+    USER_REVIEW_IKB_MESSAGE,
     PAYMENT_IKB_MESSAGE
 )
 
@@ -31,6 +33,7 @@ def main_menu_ikb(alert: bool) -> InlineKeyboardMarkup:
         )
     )
     ikb.row(InlineKeyboardButton(text=SOCIAL_IKB_MESSAGE, callback_data=SOCIAL_CALLBACK_DATA))
+    ikb.row(InlineKeyboardButton(text=USER_REVIEW_IKB_MESSAGE, callback_data=USER_REVIEW_CALLBACK_DATA))
     ikb.row(InlineKeyboardButton(text=PAYMENT_IKB_MESSAGE, callback_data=PAYMENT_CALLBACK_DATA))
 
     return ikb
