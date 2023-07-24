d06d338dc32f4a0dca0c6838519d09e946785ab7
Ivan <razgonyaev_74@mail.ru>
2023-07-24 22:12:28 +0700

    update films_genres.py

--- database/schemas/films_genres.py
+++ database/schemas/films_genres.py
@@ -10,9 +10,9 @@ class FilmsGenres(BaseModel):
     id = Column(BigInteger, primary_key=True, autoincrement=True,
                 server_default=sql.text('nextval(\'films_genres_id_seq\')'))
     # Genre id.
-    genre_id = Column(Integer, primary_key=True, nullable=False)
+    genre_id = Column(Integer, nullable=False)
     # Film info code id.
-    code_id = Column(Integer, primary_key=True, nullable=False)
+    code_id = Column(Integer, nullable=False)
     # Created film genre date.
     created_date = Column(DateTime(True), server_default=func.now())
 
